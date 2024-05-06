beb5c0071efb7a5b5a7954f07a206ea648d7f7d6
User <User@LAB2-10>
2024-05-06 14:09:02 +0300

    START DB

--- LoginDemoServer/DBScripts/Create.sql
+++ LoginDemoServer/DBScripts/Create.sql
@@ -2,6 +2,7 @@
 Go
 
 
+
 Create Table Users (
 	Email nvarchar(100) PRIMARY KEY,
 	[Password] nvarchar(20) NOT NULL,
@@ -14,5 +15,23 @@ Go
 
 INSERT INTO dbo.Users VALUES ('ofer@ofer.com', '1234', '+972526344450','15-nov-1972',1,'Ofer Zadikario')
 Go
+
+Create Table Grades(
+	[Subject] nvarchar(100) NOT NULL,
+	Score int NOT NULL,
+	[Date] DATETIME NOT NULL,
+	Email nvarchar(100) FOREIGN KEY REFERENCES Users(Email)
+)
+
+INSERT INTO dbo.Grades VALUES ('History', 96,'22-jan-1987','ofer@ofer.com')
+Go
+INSERT INTO dbo.Grades VALUES ('Math',87,'13-apr-1987','ofer@ofer.com')
+Go
+INSERT INTO dbo.Grades VALUES ('English', 100,'12-dec-1986','ofer@ofer.com')
+Go
+INSERT INTO dbo.Grades VALUES ('Computers', 92,'1-feb-1987','ofer@ofer.com')
+Go
+
+select * from Grades
 
 --scaffold-DbContext "Server = (localdb)\MSSQLLocalDB;Initial Catalog=LoginDemoDB;Integrated Security=True;Persist Security Info=False;Pooling=False;Multiple Active Result Sets=False;Encrypt=False;Trust Server Certificate=False;Command Timeout=0" Microsoft.EntityFrameworkCore.SqlServer -OutPutDir Models -Context LoginDemoDbContext -DataAnnotations -force
